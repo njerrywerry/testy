d68cb6186d26ffe8bf0d19cd5ac2601d278ddbda
Sylvia <sylviakieha@gmail.com>
2015-10-29 12:05:17 +0300

    added validations to user model and removed phone number field

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true
+  validates :age, presence: true, numericality: { only_integer: true, greater_than: 15 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151029082447) do
+ActiveRecord::Schema.define(version: 20151029085405) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "age"
-    t.string   "phone_number"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "email"
   end
 
 end
