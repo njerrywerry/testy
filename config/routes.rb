a7e10692f20ea358eab027c2be7f3169eb464ebb
Sylvia <sylviakieha@gmail.com>
2015-10-29 19:31:44 +0300

    included api routes and user serializer

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
   resources :users
 
   root 'users#index'
+
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only [:index, :create, :show, :update, :destroy]
+    end
+  end
+
 end
