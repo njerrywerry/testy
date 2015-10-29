e1157a7df3d8396f012feb81594ef68be7c4c184
Sylvia <sylviakieha@gmail.com>
2015-10-29 13:46:20 +0300

    using tests with fixtures

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,4 +10,30 @@ class UserTest < ActiveSupport::TestCase
     )
     assert !user.save
   end
+
+  test "validates attributes presence" do
+    @user1 = users(:user1)
+    @user1.name = nil
+    @user1.age = nil
+    @user1.email = nil
+
+    assert_not @user1.valid?
+  end
+
+  test "validates email uniqueness" do
+    @user1 = users(:user1)
+    @user2 = users(:user2)
+
+    @user1.email = @user2.email
+
+    assert_not @user1.valid?
+  end
+
+  test "email validity" do
+    @user1 = users(:user1)
+    @user1.email = "sly@njeri.com"
+
+    assert_match %r{^([0-9a-zA-Z]([-\.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$}, "sly@njeri.com"
+  end
+
 end
