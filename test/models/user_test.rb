972d1b86684ac0ab7a13ae43d20b4bdecf0fa58a
Sylvia <sylviakieha@gmail.com>
2015-10-29 13:50:50 +0300

    eliminating duplication with callbacks

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,6 +2,11 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
+  def setup
+    @user1 = users(:user1)
+    @user2 = users(:user2)
+  end
+
   test "saves with valid attributes" do
     user = User.new(
     name: "name",
@@ -12,7 +17,6 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "validates attributes presence" do
-    @user1 = users(:user1)
     @user1.name = nil
     @user1.age = nil
     @user1.email = nil
@@ -21,16 +25,12 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "validates email uniqueness" do
-    @user1 = users(:user1)
-    @user2 = users(:user2)
-
     @user1.email = @user2.email
 
     assert_not @user1.valid?
   end
 
   test "email validity" do
-    @user1 = users(:user1)
     @user1.email = "sly@njeri.com"
 
     assert_match %r{^([0-9a-zA-Z]([-\.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$}, "sly@njeri.com"
